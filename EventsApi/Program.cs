e5275ffb6ab10956aef3ddc9dfe548949aaadef1
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-17 23:37:24 +0700

    feat: base settings

--- EventsApi/Program.cs
+++ EventsApi/Program.cs
@@ -1,35 +1,28 @@
+using EventsApi.Models;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+builder.Services.AddCors(options =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    options.AddPolicy("AllowFrontend"
+    , policy =>
+    {
+        policy
+    .WithOrigins("http://localhost:3000"
+    ,
+    "https://myapp.example.com")
+    .AllowAnyMethod() // GET, POST, PUT, DELETE и т.д.
+    .AllowAnyHeader() // любой заголовок
+    .AllowCredentials(); // если нужно передавать/куки авторизационные заголовки
+    });
 });
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+var events = new List<EventItem>();
+var nextId = 1;
 
 app.Run();
-
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
