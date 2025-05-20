9c5d0cfc893fd8e657f553406e75d631274f54fb
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-20 21:17:06 +0700

    feat: start second project

---
+++ NotificationApi/Models/NotificationDto.cs
@@ -0,0 +1,18 @@
+﻿namespace NotificationApi.Models
+{
+    public class NotificationDto
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public DateTime EventDate { get; set; }
+        // Пустой конструктор для model binding
+        public NotificationDto() { }
+        public NotificationDto(int eventId, string title, DateTime eventDate)
+        {
+            EventId = eventId;
+            Title = title;
+            EventDate = eventDate;
+        }
+    }
+
+}
