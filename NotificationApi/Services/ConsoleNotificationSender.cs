ec2241c597f9338303b87b4a4afe2a3d845768d1
Lost-S0ul <kireev.ivan.8@gmail.com>
2025-05-21 20:34:58 +0700

    feat: sender class

---
+++ NotificationApi/Services/ConsoleNotificationSender.cs
@@ -0,0 +1,22 @@
+﻿using NotificationApi.Models;
+
+namespace NotificationApi.Services
+{
+    public class ConsoleNotificationSender : INotificationSender
+    {
+        private readonly ILogger<ConsoleNotificationSender> _logger;
+        public ConsoleNotificationSender(ILogger<ConsoleNotificationSender>
+        logger)
+        => _logger = logger;
+        public Task SendAsync(NotificationDto notification)
+        {
+            _logger.LogInformation
+            (
+            "[Console] Событие #{EventId}: {Title} в {Date}"
+            ,
+            notification.EventId, notification.Title, notification.EventDate
+            );
+            return Task.CompletedTask;
+        }
+    }
+}
